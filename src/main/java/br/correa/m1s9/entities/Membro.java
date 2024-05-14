c5714b7e26e400fdb890e0d0eeaec1b9a0624c42
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-05-14 15:38:02 -0300

    Ex1 Done

---
+++ src/main/java/br/correa/m1s9/entities/Membro.java
@@ -0,0 +1,27 @@
+package br.correa.m1s9.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "membros")
+public class Membro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 255)
+    private String endereco;
+
+    @Column(nullable = false)
+    private String telefone;
+}
---
+++ src/main/java/br/correa/m1s9/entities/Visitante.java
@@ -0,0 +1,24 @@
+package br.correa.m1s9.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "visitantes")
+public class Visitante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false)
+    private String telefone;
+}
