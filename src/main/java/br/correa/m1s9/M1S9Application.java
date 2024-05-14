f50eca12dee09a2194f76126b1c2f36298477b16
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-05-14 14:51:28 -0300

    Init

---
+++ src/main/java/br/correa/m1s9/M1S9Application.java
@@ -0,0 +1,13 @@
+package br.correa.m1s9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1S9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M1S9Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/correa/m1s9/M1S9ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.correa.m1s9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
